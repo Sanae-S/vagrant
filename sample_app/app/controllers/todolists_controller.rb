6fb019a6c43f0fefd45c0db8f21a90fac4775f52
白井紗奈絵 <shiraishanae@shiraishanaenoMacBook-Pro.local>
2020-04-15 04:16:37 +0900

    [Finish]3-5createアクション追加

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -2,4 +2,21 @@ class TodolistsController < ApplicationController
   def new
   	@list = List.new
   end
+
+  # add 3-5
+  def create
+
+  	list = List.new(list_parpams)
+
+  	list.save
+
+  	redirect_to '/top'
+  end
+
+  private
+
+  def list_parpams
+  	parpams.require(:list).permit(:title, :body)
+  end
+  
 end
