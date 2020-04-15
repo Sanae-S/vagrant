3f3661af15efe3239f78d67c1b49c04c880dc934
白井紗奈絵 <shiraishanae@shiraishanaenoMacBook-Pro.local>
2020-04-15 11:01:10 +0900

    [Finish]3-8 RuntimeError fix

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '94f4a5baab862e5876ed33db322f1eb3e3b6c5e0df5a6875e4362011cdd676fdf7597e81cb7ee695dddf3dd0dae5f35323336c604ae70829de0f842742937be1'
