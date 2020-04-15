24c00f3357a1fae3bb215f666a49893df82dd9f2
白井紗奈絵 <shiraishanae@shiraishanaenoMacBook-Pro.local>
2020-04-15 16:49:25 +0900

    [Finish]4-3 処理を追加Strong Parameters

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -11,8 +11,15 @@ class BlogsController < ApplicationController
 
 # 4-3 表示された投稿を保存
   def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
   end
 
   def edit
   end
+
+　private
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
 end
