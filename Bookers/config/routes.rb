0e3b185fe7edbff9c82c4429dd72fc8c10debf79
白井紗奈絵 <shiraishanae@shiraishanaenoMacBook-Pro.local>
2020-04-18 08:06:52 +0900

    [Update]4-8 要修正

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
 get '/' => 'books#top'
-get 'books/index' => 'books#index'
+get '/books' => 'books#index'
+post 'books_path' => 'books#create'
+post 'destroy_path' => 'books#destroy'
 resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
