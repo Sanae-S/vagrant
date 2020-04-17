6aa94eed1b7ae8cf1a8c3bcbbe8a0a2dafaf773c
白井紗奈絵 <shiraishanae@shiraishanaenoMacBook-Pro.local>
2020-04-18 05:16:42 +0900

    [Recreate]4-8 indexにちゃんと飛ぶ状態

--- Bookers/db/schema.rb
+++ Bookers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_142123) do
+ActiveRecord::Schema.define(version: 2020_04_17_200950) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
